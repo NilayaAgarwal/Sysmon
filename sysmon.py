54553ae778fd12979e1bbedb37199cdb3773dc67
NilayaAgarwal <49634326+NilayaAgarwal@users.noreply.github.com>
2020-11-25 14:10:11 +0530

    Update sysmon.py

--- sysmon.py
+++ sysmon.py
@@ -8,7 +8,7 @@ from .gather_data import sysinfo
 import pkg_resources
 
 
-def bytes_to_bit(bytes, per_second_flag=0, r=2):
+def bytes_to_bit(bytes, per_second_flag=0, r=2):                                                # for conversion of  given bytes to bit
     if bytes * 8 > 1e12:
         number_str = str(round(int(bytes) / 1e12 * 8, r))
         unit = 'Tbit'
@@ -32,7 +32,7 @@ def bytes_to_bit(bytes, per_second_flag=0, r=2):
     return number_str
 
 
-def bytes_to_byte(bytes, per_second_flag=0, r=2):
+def bytes_to_byte(bytes, per_second_flag=0, r=2):                                            # for conversion of  given bytes to byte
     if bytes > 1e12:
         number_str = str(round(int(bytes) / 1e12, r))
         unit = 'TB'
@@ -56,7 +56,7 @@ def bytes_to_byte(bytes, per_second_flag=0, r=2):
     return number_str
 
 
-def bytes_to_bibyte(bytes, per_second_flag=0, r=2):
+def bytes_to_bibyte(bytes, per_second_flag=0, r=2):                                           # for conversion of  given bytes to binary prefix of byte
     if bytes > 1024**4:
         number_str = str(round(int(bytes) / 1024**4, r))
         unit = 'TiB'
@@ -81,7 +81,7 @@ def bytes_to_bibyte(bytes, per_second_flag=0, r=2):
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
+    def runningprocess_info(self, *args, **kwargs):                                            # for details of running processes   #retrieves details from ps aux command
         super(MainWindow, self).__init__(*args, **kwargs)
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
@@ -92,7 +92,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.len_data = 60
         self.cpu_curve = []
         self.mem_curve = []
-        self.gpu_curve = []
         self.net_curve = []
         self.disk_curve = []
         self.ti = []
@@ -154,8 +153,6 @@ class MainWindow(QtWidgets.QMainWindow):
                 hbox_4 = QtGui.QGridLayout(widget_4)
                 hbox_5 = QtGui.QGridLayout(widget_5)
 
-                label_1 = QtGui.QLabel("Gpu:")
-                label_1_sub = QtGui.QLabel("% over 60 seconds")
                 label_1_sub.setFont(QtGui.QFont('Ubuntu', 7))
                 label_2 = QtGui.QLabel("Memory:")
                 label_2_sub = QtGui.QLabel("% over 60 seconds")
@@ -179,16 +176,6 @@ class MainWindow(QtWidgets.QMainWindow):
                 hbox_5.addWidget(label_5, 1, 1)
                 hbox_5.addWidget(label_6, 1, 2)
 
-                graph_w_1 = pg.GraphicsLayoutWidget()
-                graph_w_2 = pg.GraphicsLayoutWidget()
-                graph_w_3 = pg.GraphicsLayoutWidget()
-                graph_w_4 = pg.GraphicsLayoutWidget()
-
-                hbox_2.addWidget(graph_w_1, 1, 1)
-                hbox_2.addWidget(graph_w_2, 1, 2)
-                hbox_4.addWidget(graph_w_3, 1, 1)
-                hbox_4.addWidget(graph_w_4, 1, 2)
-
                 # Append for later access
                 tab_widgets.append(tab)
                 tab_widgets.append(vbox)
@@ -208,43 +195,33 @@ class MainWindow(QtWidgets.QMainWindow):
                 tab_widgets.append(label_4)
                 tab_widgets.append(label_5)
                 tab_widgets.append(label_6)
-                tab_widgets.append(graph_w_1)
-                tab_widgets.append(graph_w_2)
-                tab_widgets.append(graph_w_3)
-                tab_widgets.append(graph_w_4)
                 self.gpu_widgets.append(tab_widgets)
-            self.plot_gpuinfo()
-            self.update_gpuinfo()
             self.timer_3 = QtCore.QTimer()
             self.timer_3.timeout.connect(self.update_gpuinfo)
             self.timer_3.start(self.wait_time_ms)
 
-    def plot_gpuinfo(self,):
-        p = []
-        for gpu_ind in range(self.s.gpu_num):
-            for i in range(1, 5):
-                p.append(self.gpu_widgets[gpu_ind][-i].addPlot())
-                p[-1].setXRange(-self.len_data * self.wait_time_ms / 1000,
-                                0, padding=0)
-                p[-1].setYRange(0, 100, padding=0)
-                p[-1].enableAutoRange('xy', False)
-                p[-1].showAxis('top', show=True)
-                p[-1].showAxis('right', show=True)
-                p[-1].axes['bottom']['item'].setStyle(showValues=True)
-                p[-1].axes['top']['item'].setStyle(showValues=False)
-                p[-1].axes['left']['item'].setStyle(showValues=False)
-                p[-1].axes['right']['item'].setStyle(showValues=True)
-                p[-1].axes['right']['item'].setGrid(100)
-                p[-1].axes['top']['item'].setGrid(100)
-                p[-1].setLabel('bottom', "Seconds")
-                p[-1].setMouseEnabled(x=False, y=False)
-                p[-1].hideButtons()
-                p[-1].setMenuEnabled(False)
-                self.gpu_curve.append(p[-1].plot(pen=pg.mkPen('b', width=1),
-                                                 fillLevel=-0.3,
-                                                 brush=(50, 50, 200, 50)))
+    def update_running_processes(self,):                                                                
+        data = self.s.get_running_processes(only_usr=False)
+        numcols = len(data[0])
+        numrows = len(data)
+        self.tableWidget.setColumnCount(numcols)
+        self.tableWidget.setRowCount(numrows)
+        for row in range(numrows):
+            for column in range(numcols):
+                if column == 1 or column == 2 or column == 3:
+                    item = QtWidgets.QTableWidgetItem()
+                    item.setData(QtCore.Qt.DisplayRole,
+                                 float(data[row][column]))
+                    self.tableWidget.setItem(row, column,
+                                             QtGui.QTableWidgetItem(item))
+                else:
+                    self.tableWidget.setItem(row, column,
+                                             QtGui.QTableWidgetItem(data[row]
+                                                                    [column]))
+        self.tableWidget.setHorizontalHeaderLabels(self.headertitle)
 
-    def plot_cpuinfo(self,):
+
+    def plot_cpuinfo(self,):                                                                # for CPU details   #retrieves details from  /proc/cpuinfo
         counter = 0
         p = []
         self.cpuinfo[-1, :] = self.s.refresh_stat() * 100
@@ -279,7 +256,6 @@ class MainWindow(QtWidgets.QMainWindow):
                 counter = 0
             else:
                 counter += 1
-
         p.append(self.widget_5.addPlot())
         p[-1].setXRange(-self.len_data * self.wait_time_ms /
                         1000, 0, padding=0)
@@ -307,7 +283,21 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer_2.timeout.connect(self.update_cpuinfo)
         self.timer_2.start(self.wait_time_ms)
 
-    def plot_meminfo(self,):
+    def update_cpuinfo(self,):
+            self.cpuinfo = np.roll(self.cpuinfo, -1, axis=0)
+            self.cpuinfo[-1, :] = self.s.refresh_stat() * 100
+            for cpu in range(self.s.cpu_core_count):
+                self.cpu_curve[cpu].setData(self.x, self.cpuinfo[:, cpu + 1])
+                self.ti[0 + 2 * cpu].setText(
+                    str(round(self.s.cpu_clock[cpu] / 1000000, 2)) + 'GHz')
+                self.ti[1 + 2 * cpu].setText(
+                    str(int(self.cpuinfo[-1, cpu + 1])) + '%')
+            self.cpu_curve[cpu + 1].setData(self.x, self.cpuinfo[:, 0])
+            self.label_3.setText('Overall usage: ' +
+                                 str(round(self.cpuinfo[-1, 0], 1)) + '%')
+            self.label_7.setText(str(round(self.cpuinfo[-1, 0], 1)) + '%')
+        
+    def plot_meminfo(self,):                                                                             # for memory usage   #retrieves details from  /proc/meminfo
         p = []
         memtotal, memoccup, swaptotal, swapfree = self.s.refresh_memory()
         p.append(self.widget.addPlot())
@@ -337,8 +327,33 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_meminfo)
         self.timer.start(self.wait_time_ms)
+ 
+    
+    def update_meminfo(self,):
+            self.meminfo = np.roll(self.meminfo, -1, axis=0)
+            memtotal, memoccup, swaptotal, swapfree = self.s.refresh_memory()
+            if swaptotal != 0:
+                self.meminfo[-1, :] = np.array([(memoccup) / memtotal,
+                                                (swaptotal - swapfree)
+                                                / swaptotal]) * 100
+                self.mem_curve[1].setData(self.x, self.meminfo[:, 1])
+                self.label_6.setText(
+                    'Swap: ' + str(round((swaptotal - swapfree) / 1048576, 1)) +
+                    'GiB of ' + str(round(swaptotal / 1048576, 1)) + 'GiB used (' +
+                    str(round(self.meminfo[-1, 1], 1)) + '%)')
+            else:
+                self.meminfo[-1, :] = np.array([(memoccup) / memtotal,
+                                                0]) * 100
+                self.label_6.setText('Swap not available')
+            self.mem_curve[0].setData(self.x, self.meminfo[:, 0])
+            self.label_5.setText(
+                'Memory: ' + str(round(memoccup / 1048576, 1)) + 'GiB of ' +
+                str(round(memtotal / 1048576, 1)) + 'GiB used (' +
+                str(round(self.meminfo[-1, 0], 1)) + '%)')
 
-    def plot_netinfo(self,):
+    
+   
+    def plot_netinfo(self,):                                                                            # for network usage   #retrieves details from  /proc/net/dev
         p = []
         self.s.refresh_network()
         for adapter in range(self.s.amount_net_adater):
@@ -436,7 +451,7 @@ class MainWindow(QtWidgets.QMainWindow):
             else:
                 self.ti_net[adapter_ind].enableAutoRange('y', True)
 
-    def plot_diskinfo(self,):
+    def plot_diskinfo(self,):                                                                       # for disks usage   #retrieves details from  /proc/diskstats
         p = []
         self.s.refresh_disks()
         for disk in range(self.s.amount_disks):
@@ -514,92 +529,7 @@ class MainWindow(QtWidgets.QMainWindow):
             else:
                 self.ti_disk[disk].enableAutoRange('y', True)
 
-    def update_meminfo(self,):
-        self.meminfo = np.roll(self.meminfo, -1, axis=0)
-        memtotal, memoccup, swaptotal, swapfree = self.s.refresh_memory()
-        if swaptotal != 0:
-            self.meminfo[-1, :] = np.array([(memoccup) / memtotal,
-                                            (swaptotal - swapfree)
-                                            / swaptotal]) * 100
-            self.mem_curve[1].setData(self.x, self.meminfo[:, 1])
-            self.label_6.setText(
-                'Swap: ' + str(round((swaptotal - swapfree) / 1048576, 1)) +
-                'GiB of ' + str(round(swaptotal / 1048576, 1)) + 'GiB used (' +
-                str(round(self.meminfo[-1, 1], 1)) + '%)')
-        else:
-            self.meminfo[-1, :] = np.array([(memoccup) / memtotal,
-                                            0]) * 100
-            self.label_6.setText('Swap not available')
-        self.mem_curve[0].setData(self.x, self.meminfo[:, 0])
-        self.label_5.setText(
-            'Memory: ' + str(round(memoccup / 1048576, 1)) + 'GiB of ' +
-            str(round(memtotal / 1048576, 1)) + 'GiB used (' +
-            str(round(self.meminfo[-1, 0], 1)) + '%)')
-
-    def update_gpuinfo(self,):
-        self.gpuinfo = np.roll(self.gpuinfo, -1, axis=0)
-        data = self.s.get_nvidia_smi_info()
-        for gpu_ind in range(self.s.gpu_num):
-            num = data[gpu_ind][4]
-            if num != '-':
-                self.gpuinfo[-1, 0, gpu_ind] = int(num)
-                self.gpu_curve[3 + 4 * gpu_ind].setData(
-                    self.x, self.gpuinfo[:, 0, gpu_ind])
-            num = data[gpu_ind][5]
-            if num != '-':
-                self.gpuinfo[-1, 1, gpu_ind] = int(num)
-                self.gpu_curve[2 + 4 * gpu_ind].setData(
-                    self.x, self.gpuinfo[:, 1, gpu_ind])
-            num = data[gpu_ind][6]
-            if num != '-':
-                self.gpuinfo[-1, 2, gpu_ind] = int(num)
-                self.gpu_curve[1 + 4 * gpu_ind].setData(
-                    self.x, self.gpuinfo[:, 2, gpu_ind])
-            num = data[gpu_ind][7]
-            if num != '-':
-                self.gpuinfo[-1, 3, gpu_ind] = int(num)
-                self.gpu_curve[0 + 4 * gpu_ind].setData(
-                    self.x, self.gpuinfo[:, 3, gpu_ind])
-            self.gpu_widgets[gpu_ind][-5].setText(
-                "Memory clock: " + str(int(data[gpu_ind][8])) + 'MHz')
-            self.gpu_widgets[gpu_ind][-6].setText(
-                "Gpu clock: " + str(int(data[gpu_ind][9])) + 'MHz')
-
-    def update_cpuinfo(self,):
-        self.cpuinfo = np.roll(self.cpuinfo, -1, axis=0)
-        self.cpuinfo[-1, :] = self.s.refresh_stat() * 100
-        for cpu in range(self.s.cpu_core_count):
-            self.cpu_curve[cpu].setData(self.x, self.cpuinfo[:, cpu + 1])
-            self.ti[0 + 2 * cpu].setText(
-                str(round(self.s.cpu_clock[cpu] / 1000000, 2)) + 'GHz')
-            self.ti[1 + 2 * cpu].setText(
-                str(int(self.cpuinfo[-1, cpu + 1])) + '%')
-        self.cpu_curve[cpu + 1].setData(self.x, self.cpuinfo[:, 0])
-        self.label_3.setText('Overall usage: ' +
-                             str(round(self.cpuinfo[-1, 0], 1)) + '%')
-        self.label_7.setText(str(round(self.cpuinfo[-1, 0], 1)) + '%')
-
-    def update_running_processes(self,):
-        data = self.s.get_running_processes(only_usr=False)
-        numcols = len(data[0])
-        numrows = len(data)
-        self.tableWidget.setColumnCount(numcols)
-        self.tableWidget.setRowCount(numrows)
-        for row in range(numrows):
-            for column in range(numcols):
-                if column == 1 or column == 2 or column == 3:
-                    item = QtWidgets.QTableWidgetItem()
-                    item.setData(QtCore.Qt.DisplayRole,
-                                 float(data[row][column]))
-                    self.tableWidget.setItem(row, column,
-                                             QtGui.QTableWidgetItem(item))
-                else:
-                    self.tableWidget.setItem(row, column,
-                                             QtGui.QTableWidgetItem(data[row]
-                                                                    [column]))
-        self.tableWidget.setHorizontalHeaderLabels(self.headertitle)
-
-
+   
 def main():
     os.environ['PATH'] = ('/sbin:/usr/local/sbin:/usr/sbin:' +
                           os.environ.get('PATH'))
